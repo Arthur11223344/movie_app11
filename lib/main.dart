953900d4ee165b6478f3125de1e3d8094d1ffa5d
Arturo <arturosolisf@gmail.com>
2024-12-03 10:43:52 -0500

    Primera ejecucion1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:ejemplo04/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 void main(){
   runApp(MyApp());
 }
@@ -10,9 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "App Movie",
+      title: "App Movie Test1",
       theme: ThemeData(
         primaryColor: Colors.green,
+        textTheme: GoogleFonts.manropeTextTheme(),
       ),
       home: HomePage(),
     );
